84003b0d31f082c7869825ac6ba4fdaada053af5
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-07-11 03:57:34 +0600

    login component added

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React from 'react';
+
+const Login = (props) => {
+    return (
+        <div>
+            <h2>Login</h2>
+        </div>
+    );
+};
+
+export default Login;
